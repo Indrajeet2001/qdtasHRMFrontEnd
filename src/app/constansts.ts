12d98bb38df51a0768eaff39681a3f0678dc89b2
athrva more <moreatharva63@gmail.com>
2024-06-25 16:56:51 +0530

    Changes in constansts.ts

--- src/app/constansts.ts
+++ src/app/constansts.ts
@@ -1,5 +1,5 @@
 
-export const BASE_API_URL = 'https://qdtashrmbackend-3.onrender.com';
+export const BASE_API_URL = 'https://qdtashrmbackend.onrender.com';
 
 // export const BASE_API_URL = 'http://localhost:8181'
 
