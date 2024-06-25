e27877fc3aef6a63ed244c5d85d233048ba02254
athrva more <moreatharva63@gmail.com>
2024-06-25 17:06:22 +0530

    updated base url in  constansts.ts

--- src/app/constansts.ts
+++ src/app/constansts.ts
@@ -1,6 +1,9 @@
 
-export const BASE_API_URL = 'https://qdtashrmbackend.onrender.com';
+
+export const BASE_API_URL= 'https://qdtashrmbackend.onrender.com';
 
 // export const BASE_API_URL = 'http://localhost:8181'
+
+
 
 
